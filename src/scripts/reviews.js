a0d5fa4ba937412c1d90f75bfa5df065c47a83f0
Artem Kremlyov <gfvhhhggffghh@gmail.com>
2019-10-20 23:17:18 +0700

    Секция скиллы реализована на Vue

--- src/scripts/reviews.js
+++ src/scripts/reviews.js
@@ -26,10 +26,24 @@ new Vue({
   methods: {
     next() {
       this.$refs.flickity.next();
+      this.checkArrows()
     },
     
     previous() {
       this.$refs.flickity.previous();
+      this.checkArrows()
+    },
+    checkArrows() {
+      if  (this.$refs.flickity.selectedIndex() === 0){
+          this.$el.querySelector('.circle-btns__item--left').disabled = true;
+      }
+      else if (this.$refs.flickity.selectedIndex() === this.$refs.flickity.slides().lenght - 1){
+          this.$el.querySelector('.circle-btns__item--right').disabled = true;
+      }
+      else{
+        this.$el.querySelector('.circle-btns__item--right').disabled = false;
+        this.$el.querySelector('.circle-btns__item--left').disabled = false;
+      }
     }
   }
 });
--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -2,21 +2,30 @@ import Vue from 'vue';
 
 
 const skill ={
-    template:'#skill'
-}
+    template:"#skill",
+    props:["skillName","skillPercent"],
+    mounted(){
+       const circle = this.$refs["color-circle"]
+       const dashArray = parseInt(getComputedStyle(circle).getPropertyValue('stroke-dasharray'))
+       const percent = dashArray / 100 * (100 - this.skillPercent)
+
+       circle.style.strokeDashoffset = percent;
+    }
+};
 
 const skillsRow ={
-    template:'#skills-row',
-    component:{skill}
-}
+    template:"#skills-row",
+    components:{ skill },
+    props:["skill"]
+};
 
 new Vue({
   el:'#skills-component',
-  template:'#skills-list',
-  component:{skillsRow},
+  template:"#skills-list",
   data:() => ({
       skills: []
   }),
+  components:{ skillsRow },
   created() {
     this.skills = require('../data/skills.json');
   }
